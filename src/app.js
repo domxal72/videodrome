369cce2021a3193f19c8c0bdbcb84f0a8a140f60
dom <domxal72@gmail.com>
2020-11-07 16:12:17 +0100

    dev env setup cmd

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log('run sally run')
